84ccf749c81d6b88cdbc78485ee28b5837ea1ab8
imdw18001 <43056844+imdw18001@users.noreply.github.com>
2018-09-12 11:14:11 +0530

    forms

---
+++ WindowsFormsApplication/WindowsFormsApplication/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fname;
+            string lname;
+            string ename;
+
+            fname = textBox1.Text;
+            lname = textBox2.Text;
+            ename = textBox3.Text;
+            MessageBox.Show("Welcome "+fname+" "+lname);
+            
+        }
+    }
+}
